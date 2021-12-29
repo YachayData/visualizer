871ff2e0a0560bae4d1ff0516903beb60d76be54
cpstuardo <cpstuardo@uc.cl>
2021-12-29 16:21:23 -0300

    new test

--- src/App.test.js
+++ src/App.test.js
@@ -1,2 +1,8 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
+
+test("renders title ", () => {
+  render(<App />);
+  const title = screen.getByText(/Visualizador/i);
+  expect(title).toBeInTheDocument();
+});
